37fab043963253fd49657b24a9a9ccdb1db8ecb3
karlalan <hkiomdeagmia@hotmail.com>
2021-03-03 21:58:42 +0900

    Update README.md, provision.sh, and requirements.txt

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# analysis-api
-Django RESTful API for comprehensive analyses application
+# Analysis WEB App with Django RESTful API
+* Python 3.8
+* Django 3.0.8
+* Apache 2
+* Postgresql 12
+
+
+## Requirements
+1. [Virtualbox 6.1.16](https://www.virtualbox.org/wiki/Downloads)
+2. [Vagrant 2.2.14](https://www.vagrantup.com/downloads)
+
+## Installation from scratch
+1. `git clone https://github.com/cshimegi/analysis-api.git`
+
+2.  ```bash
+$  cd /to/Vagrantfile/path
+$  vagrant up --provision
+```
+
+3.  ```bash
+$  vagrant ssh
+$  sudo systemctl stop apache2
+$  source /env38/bin/activate
+$  sudo pip3 install -r /vagrant/requirements.txt
+$  sudo python3 /vagrant/manage.py makemigrations
+$  sudo python3 /vagrant/manage.py migrate
+$  sudo python3 /vagrant/manage.py collectstatic -c --no-input
+$  deactivate
+$  sudo systemctl start apache2
+```
+
+## Access to Django WEB
+After Vagrant is booted, you can access API page from browser.
+
+`http://192.168.12.2:8080/api/v1`
+
+## Useful Commands
+### Vagrant
+```bash
+$  vagrant up  //  to strat
+$  vagrant up --provision //  to strat and provision
+$  vagrant ssh  //  connect by SSH
+$  vagrant halt // to halt
+```
+
+### Ubuntu (bento/ubuntu-20.10)
+```bash
+$  source /env38/bin/activate // activate virtual python3 env
+$ (env38) deactivate // deactivate virtual python3 env
+```
--- vagrant-provision/provision.sh
+++ vagrant-provision/provision.sh
@@ -17,38 +17,30 @@ sudo apt -y install python3.8 python3-pip python3-venv
 echo "====================End of Python Installation========================="
 
 echo "==================Install Apache================================"
-sudo apt install -y apache2
+sudo apt install -y apache2 apache2-dev
 sudo apt install libapache2-mod-wsgi-py3
-sudo pip3 install mod_wsgi==4.7.1
+echo "====================End of Apache========================="
+
+echo "==================Process Apache Settings================================"
 sudo a2enmod wsgi
 sudo cp /etc/apache2/apache2.conf /etc/apache2/apache2.conf.orig
-sudo cp /vagrant/vagrant-provision/apache2.conf /etc/apache2/apache2.conf.orig
+sudo cp /vagrant/vagrant-provision/apache2.conf /etc/apache2/apache2.conf
 sudo cp /vagrant/vagrant-provision/django.conf /etc/apache2/sites-available/django.conf
 sudo cp /vagrant/vagrant-provision/ports.conf /etc/apache2/ports.conf
 sudo a2dissite 000-default
 sudo a2ensite django
 sudo systemctl restart apache2
-echo "====================End of Apache========================="
+echo "==================End of Apache Settings================================"
 
 echo "==================Install Cron================================"
 sudo apt install cron
 echo "====================End of Cron Installation========================="
 
+echo "==================Install Django and Mod_wsgi================================"
+sudo pip3 install Django==3.0.8
+echo "====================End of Django and Mod_wsgi========================="
+
 echo "==================Create Python Virtual Env================================"
 # create virtual python env
 sudo python3 -m venv /env38
 echo "====================End of Python virtual Env Creation========================="
-
-echo "==================Install Required Python Packages================================"
-source /env38/bin/activate
-# wheel for package dependency; psycopg2-binary for ubuntu / psycopg2 for windows
-# Install required packages
-sudo pip3 install -r /vagrant/requirements.txt
-echo "==================End of Installing Required Python Packages======================"
-
-echo "==================Django Migration Start======================"
-sudo python3 /vagrant/manage.py makemigrations
-sudo python3 /vagrant/manage.py migrate
-sudo python3 /vagrant/manage.py collectstatic -c --no-input
-deactivate
-echo "==================Django Migration End======================"
